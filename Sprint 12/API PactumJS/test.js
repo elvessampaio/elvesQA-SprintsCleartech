df153dfbdcf8d344207f39894783efc97ca6f13e
Elves Sampaio <emoura@cleartech.dev>
2023-08-15 17:52:55 -0300

    Criando testes

--- Sprint 12/API PactumJS/test.js
+++ Sprint 12/API PactumJS/test.js
@@ -1,7 +1,38 @@
 const { spec } = require('pactum');
 
-it('should get a response with status code 200', async () => {
-  await spec()
-    .get('https://www.servicos.gov.br/api/v1/servicos/10045')
-    .expectStatus(200);
+it('Teste 1 GET', async () => {
+  let {body, responseTime, statusCode} = await spec()
+    .get('http://numbersapi.com/1993/year')
+    .expectResponseTime(1000)
+    .expectStatus(200)
+    ;
+    console.log(body, 'Tempo de requisição:', responseTime, '|', 'Status Code:', statusCode)
+});
+
+it('Teste 2 - POST', async () => {
+  let {body, responseTime, statusCode} = await spec()
+    .post('https://jsonplaceholder.typicode.com/posts')
+    .withBody({
+      "nome": "Elves",
+      "idade": 29,
+      "id": ""
+    })
+    .expectResponseTime(1000)
+    .expectStatus(201)
+    ;
+    console.log(body, 'Tempo de requisição:', responseTime, '|', 'Status Code:', statusCode)
+});
+
+it('Teste 3 - PUT', async () => {
+  let {body, responseTime, statusCode} = await spec()
+    .put('https://jsonplaceholder.typicode.com/posts/1')
+    .withBody({
+      "nome": "Elves",
+      "idade": 30,
+      "id": ""
+    })
+    .expectResponseTime(1000)
+    .expectStatus(200)
+    ;
+    console.log(body, 'Tempo de requisição:', responseTime, '|', 'Status Code:', statusCode)
 });
