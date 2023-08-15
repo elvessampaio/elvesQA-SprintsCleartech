14e1b71cc3d5b8e9c35742ea9542eb2aed65a76f
Elves Sampaio <emoura@cleartech.dev>
2023-08-15 19:25:21 -0300

    Adicionado mais testes

--- Sprint 12/API PactumJS/test.js
+++ Sprint 12/API PactumJS/test.js
@@ -4,8 +4,8 @@ it('Teste 1 GET', async () => {
   let {body, responseTime, statusCode} = await spec()
     .get('http://numbersapi.com/1993/year')
     .expectResponseTime(1000)
-    .expectStatus(200)
-    ;
+    .expectStatus(200);
+    
     console.log(body, 'Tempo de requisição:', responseTime, '|', 'Status Code:', statusCode)
 });
 
@@ -15,11 +15,10 @@ it('Teste 2 - POST', async () => {
     .withBody({
       "nome": "Elves",
       "idade": 29,
-      "id": ""
     })
     .expectResponseTime(1000)
     .expectStatus(201)
-    ;
+    
     console.log(body, 'Tempo de requisição:', responseTime, '|', 'Status Code:', statusCode)
 });
 
@@ -33,6 +32,38 @@ it('Teste 3 - PUT', async () => {
     })
     .expectResponseTime(1000)
     .expectStatus(200)
-    ;
-    console.log(body, 'Tempo de requisição:', responseTime, '|', 'Status Code:', statusCode)
+    .expectBody({nome: 'Elves', idade: 30, id: 1})
+
+  console.log(body, 'Tempo de requisição:', responseTime, '|', 'Status Code:', statusCode);
+});
+
+it('Teste 4 DEL (Sem informar ID)', async () => {
+  let {body, responseTime, statusCode} = await spec()
+    .delete('https://jsonplaceholder.typicode.com/posts/')
+    .expectResponseTime(1000)
+    .expectStatus(404);
+    
+    console.log('Tempo de requisição:', responseTime, '|', 'Status Code:', statusCode)
+});
+
+it('Teste 5 DEL (informando o ID)', async () => {
+  let {body, responseTime, statusCode} = await spec()
+    .delete('https://jsonplaceholder.typicode.com/posts/1')
+    .expectResponseTime(1000)
+    .expectStatus(200);
+    
+    console.log('Tempo de requisição:', responseTime, '|', 'Status Code:', statusCode)
+});
+
+it('Teste 6 - POST em URL que aceita apenas GET', async () => {
+  let {body, responseTime, statusCode} = await spec()
+    .post('http://numbersapi.com/1993/year')
+    .withBody({
+      "nome": "Elves",
+      "idade": 29,
+    })
+    .expectResponseTime(1000)
+    .expectStatus(404)
+    
+    console.log('Tempo de requisição:', responseTime, '|', 'Status Code:', statusCode)
 });
